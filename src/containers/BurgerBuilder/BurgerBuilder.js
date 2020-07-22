70b37cd41cc907bae0b6e6e8402f493716179e67
Alex <alexnem@inbox.ru>
2020-07-22 22:28:39 +0300

    added calculating the ingredient sum dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/auxComponent';
+import { Burger } from '../../components/Layout/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      cheese: 0,
+      bacon: 0,
+      salad: 0,
+      meat: 0
+    }
+  }
+
   render() {
     return (
       <Aux>
-          <div>Burger</div>
+          <Burger ingredients={this.state.ingredients} />
           <div>Build Controls</div>
       </Aux>
     );
