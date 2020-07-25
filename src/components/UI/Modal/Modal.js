01aed0d2c87cab1e7caa36507efe81f13c4608a5
Alex <alexnem@inbox.ru>
2020-07-25 20:20:04 +0300

    added modal component

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Modal.module.css';
+import Aux from '../../../hoc/auxComponent';
+import { Backdrop } from '../Backdrop/Backdrop';
+
+export const Modal = (props) => {
+  return (
+    <Aux>
+      <Backdrop
+        show={ props.show }
+        clicked={props.modalClosed}
+      />
+      <div
+        className={ classes.Modal }
+        style={ {
+          transform: props.show ? 'translateY(0)' : 'translateY(-180vh)',
+          opacity: props.show ? '1' : '0',
+        } }
+      >
+        { props.children }
+      </div>
+    </Aux>
+  );
+};
