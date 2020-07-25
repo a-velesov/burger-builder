e6aed083b87719ff0b65812bc325e2edd482ce0a
Alex <alexnem@inbox.ru>
2020-07-25 15:36:30 +0300

    add & remove ingredient function; disabled logic

--- src/components/Layout/Burger/BuildControls/BuildControls.js
+++ src/components/Layout/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,13 @@ export const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
       {controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
       ))}
     </div>
   );
