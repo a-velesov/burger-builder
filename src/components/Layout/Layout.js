38583558cc836b38397c0784925374f65ab7abd0
Alex <alexnem@inbox.ru>
2020-07-26 12:30:18 +0300

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/auxComponent';
+import classes from './Layout.module.css'
+import { Toolbar } from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => {
   return (
     <Aux>
-      <div>Toolbar</div>
-      <main>
+      <Toolbar />
+      <main className={classes.Content}>
         { props.children }
       </main>
     </Aux>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Toolbar.module.css';
+
+export const Toolbar = (props) => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>MENU</div>
+      <div>LOGO</div>
+      <nav>
+
+      </nav>
+    </header>
+  );
+};
