074b00aef32728e6365164308af6d5fd22f7ab1d
Alex <alexnem@inbox.ru>
2020-08-11 14:49:57 +0300

    refactor css

--- src/components/Burger/SelectionControls/SelectionControls.js
+++ src/components/Burger/SelectionControls/SelectionControls.js
@@ -78,18 +78,19 @@ class SelectionControls extends Component {
     ));
 
     //display warning message when ingredients are greater than 10
-    const warningMessage =
-            totalIngredients >= 10 ? (
-              <p className={ classes.Warning }>
-          You've reached your total ingredient limit.
-        </p>
-            ) : null;
+
 
     return (
       <div className={ classes.SelectionControls }>
         <h2 className={ classes.TotalPrice }>${ totalPrice.toFixed(2) }</h2>
         <p>Total Ingredients Used: { totalIngredients }/10</p>
-        { warningMessage }
+
+        { totalIngredients >= 10 ? (
+          <p className={ classes.Warning }>
+          You've reached your total ingredient limit.
+        </p>
+        ) : null }
+
         <div className={ classes.SelectionContainer }>{ displayControls }</div>
         <Button
           type="secondary"
